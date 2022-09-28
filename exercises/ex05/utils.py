2d8ecc492e09ad829216e1fda6fd799d776ae944
Max Li <mgli@unc.edu>
2022-09-28 00:46:35 -0400

    ex05_v1

---
+++ exercises/ex05/utils.py
@@ -0,0 +1,36 @@
+"""ex05 function skeletons."""
+__author__ = "730571410"
+
+
+def only_evens(xs: list[int]) -> list[int]:
+    """Returns a list containing only even elements of the input list."""
+    evens: list[int] = []
+    for x in xs:
+        if x % 2 == 0:
+            evens.append(x) 
+    return evens
+            
+
+def concat(xs: list[int], ys: list[int]):
+    """Returns a list containing the first list followed by the second list."""
+    new_list: list[int] = []
+    for x in xs:
+        new_list.append(x)
+    for y in ys:
+        new_list.append(y)
+    return new_list
+    
+
+    
+def sub(xs: list[int], start: int, end:int):
+    """Returns a list that is a subset of a list between a specified start index and end index - 1."""
+    i: int = start
+    new_list: list[int] = []
+    if start < 0:
+        i = 0
+    if end > len(xs):
+        end = len(xs)
+    while i >= start and i < end:
+        new_list.append(xs[i])
+        i += 1
+    return new_list
---
+++ exercises/ex05/utils_test.py
@@ -0,0 +1,62 @@
+"""Tests for ex05 functions."""
+__author__ = "730571410"
+
+from exercises.ex05.utils import only_evens
+
+
+def test_only_evens_edge_case_no_evens() -> None:
+    xs: list[int] = [3, 7, 5]
+    assert only_evens(xs) == []
+
+def test_only_evens_use_case_many_items() -> None:
+    xs: list[int] = [1, 2 , 3]
+    assert only_evens(xs) == [2]
+
+def test_only_evens__use_case_many_items_again() -> None:
+    xs: list[int] = [3, 4, 6, 7, 6]
+    assert only_evens(xs) == [4, 6, 6]
+
+
+from exercises.ex05.utils import concat
+
+
+def test_concat_edge_case_empty_lists() -> None:
+    xs: list[int] = []
+    ys: list[int] = []
+    assert concat(xs, ys) == []
+
+
+def test_concat_use_case_many_items() -> None:
+    xs: list[int] = [1, 3]
+    ys: list[int] = [2, 4]
+    assert concat(xs, ys) == [1, 3, 2, 4]
+
+
+def test_concat_use_case_many_items_again() -> None:
+    xs: list[int] = [10, 110, 190, 2000]
+    ys: list[int] = [3, 55]
+    assert concat(xs, ys) == [10, 110, 190, 2000, 3, 55]
+
+
+from exercises.ex05.utils import sub
+
+
+def test_sub_edge_case_negative_start_and_longer_end() -> None:
+    xs: list[int] = [1, 2, 3, 4]
+    start: int = -2
+    end: int = 7
+    assert sub(xs, start, end) == [1, 2, 3, 4]
+
+
+def test_sub_use_case_many_items() -> None:
+    xs: list[int] = [1, 2, 3, 4]
+    start: int = 1
+    end: int = 3
+    assert sub(xs, start, end) == [2, 3]
+
+
+def test_sub_use_case_many_items_again() -> None:
+    xs: list[int] = [100, 250, 340, 490, 709, 800]
+    start: int = 2
+    end: int = 5
+    assert sub(xs, start, end) == [340, 490, 709]    
